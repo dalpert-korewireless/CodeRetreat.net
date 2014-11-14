d015a593e3b0c1fbea69fab62c08da7a4d172236
David Alpert <david@spinthemoose.com>
2014-11-14 13:04:37 -0600

    empty FSharp project with both FSharp and CSharp unit test beds

---
+++ CodeRetreat.CSharpTests/TestFixture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CodeRetreat.CSharpTests
+{
+    public class TestFixture
+    {
+        [Test]
+        public void can_run_a_test()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ CodeRetreat.FSharpTests/Library1.fs
@@ -0,0 +1,18 @@
+﻿namespace CodeRetreate.FSharp.Tests
+
+open System
+open NUnit.Framework
+open FsUnit
+
+module _Extensions =
+
+    type System.String with
+        member this.NormalizeLineEndings() = this.Replace(@"\r\n","\n")
+
+type ``When running a test``() =
+
+    let input = true
+
+    [<Test>] member x.
+     ``can run a test``() =
+        input |> should equal true
