a926373390837001fef61a5059ab49a1087ba8ae
David Alpert <david@spinthemoose.com>
2014-11-14 12:50:28 -0600

    initial c# class lib and c# test project

---
+++ CodeRetreat.Tests/TestFixture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace CodeRetreat.Tests
+{
+    public class TestFixture
+    {
+        [Test]
+        public void can_run_a_test()
+        {
+            Assert.True(true);
+        }
+    }
+}
